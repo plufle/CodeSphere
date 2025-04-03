823d98c9b584ef28ab358d0575fbb3d86532f4ae
plufle <sarveshsss2005@gmail.com>
2025-04-03 17:38:27 +0530

    Completed Theme Switcher

--- Frontend/script.js
+++ Frontend/script.js
@@ -6,7 +6,7 @@ require.config({
 let editor;
 require(["vs/editor/editor.main"], function () {
   editor = monaco.editor.create(document.getElementById("codeEditor"), {
-    value: "print('Hello World!')\n",
+    value: "print('This is a python Code!')\n",
     language: "python",
     theme: "vs-dark",
     automaticLayout: true,
@@ -21,21 +21,21 @@ require(["vs/editor/editor.main"], function () {
 
     switch (selectedLanguage) {
       case "python":
-        editor.setValue("print('Hello World!')\n");
+        editor.setValue("print('This is a python Code!')\n");
         break;
       case "c":
         editor.setValue(
-          `#include <stdio.h>\n\nint main() {\n    printf("Hello, World!\\n");\n    return 0;\n}`
+          `#include <stdio.h>\n\nint main() {\n    printf("This is a C Code!\\n");\n    return 0;\n}`
         );
         break;
       case "cpp":
         editor.setValue(
-          `#include <iostream>\nusing namespace std;\n\nint main() {\n    cout << "Hello, World!" << endl;\n    return 0;\n}`
+          `#include <iostream>\nusing namespace std;\n\nint main() {\n    cout << "This is a C++ Code!" << endl;\n    return 0;\n}`
         );
         break;
       case "java":
         editor.setValue(
-          `public class Main {\n    public static void main(String[] args) {\n        System.out.println("Hello, World!");\n    }\n}`
+          `public class Main {\n    public static void main(String[] args) {\n        System.out.println("This is a Java Code!");\n    }\n}`
         );
         break;
       default:
@@ -115,3 +115,54 @@ function download() {
 
   URL.revokeObjectURL(url);
 }
+
+let isDark = true;
+document.querySelector(".icon-container").addEventListener("click", () => {
+  document.querySelector(".icon-dark").classList.toggle("hidden");
+  document.querySelector(".icon-light").classList.toggle("hidden");
+
+  isDark = !isDark;
+
+  if (isDark) {
+    document.querySelector(".body").classList.add("dark");
+    document.querySelector(".header").classList.add("dark");
+    document.querySelector(".header").classList.remove("light");
+    document.querySelector(".options-bar").classList.add("dark");
+    document.querySelector(".options-bar").classList.remove("light");
+    document.querySelector(".language-select").classList.add("dark");
+    document.querySelector(".language-select").classList.remove("light");
+    document.querySelector(".font-size-input").classList.add("dark");
+    document.querySelector(".font-size-input").classList.remove("light");
+    document.querySelectorAll(".button").forEach((e) => {
+      e.classList.add("dark");
+      e.classList.remove("light");
+    });
+    editor.updateOptions({ theme: "vs-dark" });
+    document.querySelector(".editor").classList.remove("light");
+    document.querySelector(".editor").classList.add("dark");
+
+    document.querySelector(".output").classList.remove("light");
+    document.querySelector(".output").classList.add("dark");
+  } else {
+    document.querySelector(".body").classList.remove("dark");
+    document.querySelector(".header").classList.remove("dark");
+    document.querySelector(".header").classList.add("light");
+    document.querySelector(".options-bar").classList.remove("dark");
+    document.querySelector(".options-bar").classList.add("light");
+    document.querySelector(".language-select").classList.remove("dark");
+    document.querySelector(".language-select").classList.add("light");
+    document.querySelector(".font-size-input").classList.remove("dark");
+    document.querySelector(".font-size-input").classList.add("light");
+    document.querySelectorAll(".button").forEach((e) => {
+      e.classList.remove("dark");
+      e.classList.add("light");
+    });
+
+    editor.updateOptions({ theme: "vs" });
+    document.querySelector(".editor").classList.add("light");
+    document.querySelector(".editor").classList.remove("dark");
+
+    document.querySelector(".output").classList.add("light");
+    document.querySelector(".output").classList.remove("dark");
+  }
+});
